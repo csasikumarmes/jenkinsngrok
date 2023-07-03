0a7b09f560c902a1276e1730230497d77fc102d8
csasikumarmes <137702332+csasikumarmes@users.noreply.github.com>
2023-07-03 22:31:29 +0530

    Add files via upload

---
+++ BasicProgram.java
@@ -0,0 +1,17 @@
+package org.test;
+
+public class BasicProgram {
+	
+	private void courseDetails() {
+	System.out.println("JAVA Basics Class");
+	}
+	
+	
+	public static void main(String[] args) {
+		// className obj = new ClassName();
+		BasicProgram b = new BasicProgram();
+		b.courseDetails();
+		
+	}
+
+}
